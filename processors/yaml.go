0655a35aab00172cb867cc7c7da0ea752cdc3c38
Alexander Solovyov <alexander@solovyov.net>
2018-11-05 14:56:51 +0200

    those comments are useless and had mistakes in them

--- processors/yaml.go
+++ processors/yaml.go
@@ -6,31 +6,25 @@ import (
 	gostatic "github.com/piranha/gostatic/lib"
 )
 
-// YamlProcessor is empty struct
 type YamlProcessor struct {
 }
 
-// NewYamlProcessor is constructor for YamlProcessor
 func NewYamlProcessor() *YamlProcessor {
 	return &YamlProcessor{}
 }
 
-// Process is function for runnig ProcessYaml
 func (p *YamlProcessor) Process(page *gostatic.Page, args []string) error {
 	return ProcessYaml(page, args)
 }
 
-// Description is function for write description
 func (p *YamlProcessor) Description() string {
 	return "read config from content using yaml format"
 }
 
-// Mode is work mode for this processor
 func (p *YamlProcessor) Mode() int {
 	return gostatic.Pre
 }
 
-// ProcessYaml is function for proccess yaml header
 func ProcessYaml(page *gostatic.Page, args []string) error {
 	r, err := regexp.Compile(`(?sm)^---\r?\n(.*)^---\r?\n(.*)`)
 	if err != nil {
